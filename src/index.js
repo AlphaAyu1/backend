7065a798c85f0dd5f847ceef35069c4672446ef5
AlphaAyu1 <meayushman2@gmail.com>
2024-02-04 18:29:44 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from './db/ind.js';
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{app.on("error",(error)=>{
+    console.log("error", error)
+    throw error
+})
 
-/*
-import  express from 'express';
-const app=express();
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at : ${process.env.PORT}`)
+    })
+})
 
-;(async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, DB_NAME)
-        app.on("error",(error)=>{
-            console.log("error",console.error())
-            throw error
-        })
+.catch((error)=>{
+    console.log("MongoDB connect failed", error)
+})
 
-        app.listen(process.env.PORT,()=>{
-            console.log("app is listening on: "+ process.env.PORT)
-        })
-    }
-    catch(error){
-        console.log("error",error)
-        throw err
-    }
-})()
-*/
--- src/utils/asyncHandeler.js
+++ src/utils/asyncHandeler.js
@@ -1,8 +1,21 @@
-const asyncHandeler=(requesthandeler)=>{
-    (req, res, next)=>{
-        Promise.resolve(requesthandeler(req,res,next)).catch((err)=>next(err))
-    }
+const asyncHandeler= (fun)=>{
+return (req,res,next)=>{
+    Promise.resolve(fun(req,res,next)).catch((error)=>next(error))
+}
 }
 
-
 export {asyncHandeler}
+
+/*
+const asyncHandeler2=(fun)=>async (req, res,next)=>{
+    try{
+        await fun(req,res,next)
+    }
+    catch(error){
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+*/
